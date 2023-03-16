a0b81476be703f7209a2b2d976b9fd89cef7e655
петрушка <parsley69@mail.ru>
2023-03-16 10:14:53 +0300

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+
+namespace Maui;
+{
+   public partial class App : Application
+   {
+     public const string DATABASE_NAME = "friends.db";
+     public static FriendRepository database;
+     public static FriendRepository Database
+     {
+        get
+        {
+            if (database == null)
+            {
+                database = new FriendRepository(
+                    Path.Combine(
+                        Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), DATABASE_NAME));
+            }
+            return database;
+        }
+     }
+     public App()
+     {
+        InitializeComponent();
+
+        MainPage = new NavigationPage(new MainPage());
+     }
+     protected override void OnStart()
+     {
+     }
+     protected override void OnSleep()
+     {
+     }
+     protected override void OnResume()
+     {
+     }
+   }
+}
+
---
+++ MauiProgram.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Maui;
+{
+    public partial class MainPage : ContentPage
+    {
+       public MainPage()
+       {
+           InitializeComponent();
+       }
+       protected override void OnAppearing()
+        {
+           friendsList.ItemsSource = App.Database.GetItems();
+           base.OnAppearing();
+       }
+    // обработка нажатия элемента в списке
+       private async void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+       {
+          Friend selectedFriend = (Friend)e.SelectedItem;
+          FriendPage friendPage = new FriendPage();
+          friendPage.BindingContext = selectedFriend;
+          await Navigation.PushAsync(friendPage);
+       }
+    // обработка нажатия кнопки добавления
+       private async void CreateFriend(object sender, EventArgs e)
+       {
+          Friend friend = new Friend();
+          FriendPage friendPage = new FriendPage();
+          friendPage.BindingContext = friend;
+          await Navigation.PushAsync(friendPage);
+       }
+    }
+}
